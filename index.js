f36097f56a9ca39d78ffae983cd457dde56baeea
hammedt20 <hammedt20@gmail.com>
2025-11-07 01:56:03 -0600

    fixed cron-job error

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ cron.schedule("0 9 * * *", () => {
   runTVLCheck();
 });
 
-app.get("/health", (req, res) => res.send("TVL Bot Alive 🚀"));
+app.get("/health", (req, res) => rres.status(200).send('OK'));
 app.listen(process.env.PORT || 3000);
